698fbfce7bb12ae2bcfdc5e82855130b8d124385
RichardChambaS <richard.ariel64@gmail.com>
2024-11-19 17:27:53 -0500

    primer commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Proyecto
 UNL proyect
+valemos madres
---
+++ src/ProjectoF.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class ProjectoF {
+    //Proyecto avance
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("-----GESTOR DE TAREAS-----");   //menu inicial
+
+            
+        
+            System.out.println("Buenos dias usuario, que es lo que necesita el dia de hoy?");
+            System.out.println("1:Agregar nueva tarea");
+            System.out.println("2:Modificar tareas ingresadas");
+            System.out.println("3:Eliminar tarea");
+            System.out.println("4:Revisar estado de tareas agregadas");
+            int menuselect = sn.nextInt();
+            switch (menuselect) {
+                case 1:
+                
+                    break;
+                case 2:
+                
+                    break;
+                case 3:
+                
+                    break;
+                case 4:
+                
+                    break;
+                case 5:
+                
+                    break;
+        
+            default:
+                break;
+        }
+        sn.close();
+    
+    
+
+    }
+}
